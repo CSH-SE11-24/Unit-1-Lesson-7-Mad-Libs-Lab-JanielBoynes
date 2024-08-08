3740924aba246fdb4422046a03baa2493eae7ed5
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 16:03:57 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// Console log a welcome message with instructions to mad libs
+
+console.log("welcome ")
+
+// Prompt the user to enter parts of speech for mad libs
+// You should be asking for at least 4 words
+let adjective = prompt("Give me an adjective")
+
+
+// Create an array that stores the values the user entered
+let array = [adjective]
+// Push the user inputs to the array
+
+
+// Console log the story with the array values passed into the blanks
+// Make sure you are referencing the values of the array (rather than the user inputs themselves)
+// You can use any type of string manipulation that feels more intuitive
+
+
+
+
+
+
+
+
